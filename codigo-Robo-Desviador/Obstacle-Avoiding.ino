96d6072f84f3bce4f965feb7b611402e8658c21e
Marzano <80858692+Maarzano@users.noreply.github.com>
2024-10-01 19:47:36 -0300

    mudando código por inteiro
    ocorreu um erro e tivemos que re fazer o código

--- codigo-Robo-Desviador/Obstacle-Avoiding.ino
+++ codigo-Robo-Desviador/Obstacle-Avoiding.ino
@@ -1,95 +1,69 @@
-// Define os pinos dos motores
-#define LEFT_MOTOR_BACKWARD 5
-#define LEFT_MOTOR_FORWARD 6
-#define RIGHT_MOTOR_BACKWARD 7
-#define RIGHT_MOTOR_FORWARD 8
+int distancia = 0;
 
-// Define os pinos do sensor ultrassônico
-#define ECHO_PIN A4
-#define TRIGGER_PIN A5
-
-#define SPEED_INCREMENT 2
-#define INITIAL_MOTOR_SPEED 80
-
-int motorSpeed = INITIAL_MOTOR_SPEED; // Variável para velocidade inicial do motor
-
-const int MIN_DISTANCE = 15; // Distância mínima para desvio em cm
-
-void setup() {
-  // Define os pinos do motor como saída
-  pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
-  pinMode(LEFT_MOTOR_BACKWARD, OUTPUT);
-  pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
-  pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
-
-  // Define os pinos do sensor ultrassônico como entrada e saída
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-
-  // Inicializa a comunicação serial
-  Serial.begin(9600);
+long lerDistanciaUltrassonica(int pinoTrigger, int pinoEcho)
+{
+  pinMode(pinoTrigger, OUTPUT);
+  digitalWrite(pinoTrigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinoTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinoTrigger, LOW);
+  pinMode(pinoEcho, INPUT);
+  return pulseIn(pinoEcho, HIGH);
 }
 
-void loop() {
-  long distance = measureDistance(); // Mede a distância do objeto mais próximo
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
 
-  if (distance < MIN_DISTANCE) {
-    motorSpeed = INITIAL_MOTOR_SPEED;
-    moveBackward();
+void loop()
+{
+  distancia = 0.01723 * lerDistanciaUltrassonica(13, 12);
+  Serial.println(distancia);
+  
+  if (distancia < 15) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
     delay(500);
-    stopMotors();
+    pararMotores();
     delay(150);
-    turnRight();
+    virarDireita();
     delay(600);
-    stopMotors();
+    pararMotores();
     delay(150);
-  } else {
-    moveForwardFull();
+    moverParaFrente();
+  } 
+  else {
+    moverParaFrente();
   }
 
-  // Imprime a distância medida no monitor serial
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  delay(10);
 }
 
-void stopMotors() {
-  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
-  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
+void pararMotores() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
 }
 
-void moveForwardFull() {
-  digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
-  digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
-  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
+void moverParaFrente() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
 }
 
-void moveBackward() {
-  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
-  digitalWrite(LEFT_MOTOR_BACKWARD, HIGH);
-  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
-}
-
-void turnRight() {
-  digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
-  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
-  digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
-}
-
-long measureDistance() { // Método de medição de distância
-  delay(50);
-  long duration, distance;
-  digitalWrite(TRIGGER_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration / 58; // Converte a duração para distância
-  return distance;
+void virarDireita() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
 }
